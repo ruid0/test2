31cf5b4189e4c8816845a4cae6fb97be10bcb88a
Igor Bondarenko <ibondarenko@reebee.com>
2022-01-19 17:50:20 -0500

    :sparkles: Add link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Welcome to my blog</h1>
+    <>
+      <h1>Welcome to my blog</h1>
+      <Link href="/extra">Extra</Link>
+    </>
   );
 }
